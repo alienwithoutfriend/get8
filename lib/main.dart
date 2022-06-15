9fee441c3a8acdfbc0781d3bf9d1b91a7ee3451d
alienwithoutfriend <68382753+alienwithoutfriend@users.noreply.github.com>
2022-06-15 21:11:00 +0700

    commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp( MyApp());
+}
+class MyApp extends StatelessWidget {
+  var count = 0.obs;
+  void add(){
+    count++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+        ),
+        body: Center(
+          child: Obx(()=>Text(
+            "angka $count",
+            style: TextStyle(
+fontSize:  35,
+            ),)),
+        ),
+        
+        floatingActionButton: FloatingActionButton(onPressed: () => add()),
+      ),
+    );
+  }
+}
